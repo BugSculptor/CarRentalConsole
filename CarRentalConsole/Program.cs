62b284736ef5898795e2cda24f311003a07a9f03
Edyta <BugSculptorNew@gmail.com>
2024-09-29 16:53:47 +0200

    Add project files.

---
+++ CarRentalConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CarRentalConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateBanner();
+            Console.WriteLine("...");
+
+
+
+
+        }
+
+
+        public void GenerateBanner()
+        {
+
+        }
+    }
+}
