c192126d3ce11f30dca4a7c73fe54921261f07b4
Edyta <BugSculptorNew@gmail.com>
2024-10-13 20:41:29 +0200

    zapisuje i odczytuje z pliku nie jestem pewna co do edycji

--- CarRentalConsole/CarManager.cs
+++ CarRentalConsole/CarManager.cs
@@ -11,13 +11,17 @@ using System.Xml.Linq;
 
 using System.Text.Json;
 using System;
+using System.Globalization;
 
 namespace CarRentalConsole
 {
+   
+
     internal class CarManager
     {
         public static void Main(string[] args)
         {
+           
             int choice;
             do
             {
@@ -42,18 +46,53 @@ namespace CarRentalConsole
                         case 4:
                             BorrowCar();
                             break;
+                        case 5:
+                            EditCar();
+                            break;
+
                         default:
                             break;
                     }
-
-
             }
             while (choice != 0);
+        }
 
+        private static void EditCar()
+        {
+
+            //1. podaj właściwości
+            Console.WriteLine("Wprowadż właściwości pojazu oddzielając je średnikiem: \nprzykład: Fiat;Tipo;benzyna;1");
+            string line = Console.ReadLine();
+            string[] vehicleToSearch = line.Split(";");
+            if(vehicleToSearch.Length != 4)
+            {
+                Console.WriteLine("Podano nieprawidlowe wartości!");
+            }
+            else
+            {
+                                                        //2. wyszukaj ID
+                int idVehicle = SearcherVehicleId("VehicleList.txt", vehicleToSearch);
+                Console.WriteLine($"ID pasującego rekordu: {idVehicle}");
+
+                //3. edytuj wg ID
+                DeleteCar("VehicleList.txt", string.Concat(idVehicle.ToString(),";", line));
+                OverrideVehicle("VehicleList.txt", idVehicle);
+            }
             
+           
+
+        }
+
+        private static void OverrideVehicle(string file, int id_deletedVehicle)
+        {
+            AddNewCar(id_deletedVehicle);
+
+           
 
 
+            //dodanie na końcu i sortowanie?
 
+            throw new NotImplementedException();
         }
 
         private static void BorrowCar() //wypozyczenie
@@ -61,12 +100,21 @@ namespace CarRentalConsole
             throw new NotImplementedException();
         }
 
-        private static void DeleteCar() //usunięcie
+        private static void DeleteCar(string file = "", string value = "") //usunięcie
         {
-            throw new NotImplementedException();
+            string fileName = file; // nazwa pliku
+            string textToRemove = value; // tekst do usunięcia
+
+            // Odczytanie wszystkich linii z pliku
+            string[] lines = File.ReadAllLines(fileName);
+            // Filtrowanie linii, które nie zawierają tekstu do usunięcia
+            var updatedLines = lines.Where(line => !line.Contains(textToRemove)).ToArray();
+            // Zapisanie zaktualizowanych linii do pliku (nadpisanie pliku)
+            File.WriteAllLines(fileName, updatedLines);
+            Console.WriteLine($"Linia zawierająca tekst '{textToRemove}' została usunięta.");
         }
 
-        private static void AddNewCar() //dodawanie
+        private static void AddNewCar(int iD = 0) //dodawanie
         {
             Console.WriteLine("Dostępne typy pojazdu: ");
             Console.WriteLine("Podaj typ: ");
@@ -77,7 +125,7 @@ namespace CarRentalConsole
             
             CarTypes carType = (CarTypes)carTypeUser;
             
-            Console.WriteLine($"Wybrałes: {carType}");
+            //Console.WriteLine($"Wybrałes: {carType}");
 
             Vehicle? vehicle = null; //bo jest dafaul i ona nie zwraca...
             switch (carType)
@@ -110,29 +158,38 @@ namespace CarRentalConsole
             Console.Write("Podaj pojemność silnika: ");
             vehicle.Capacity = int.Parse(Console.ReadLine());
 
-            var repository = new VehicleRepository();
-            //repository.Add(vehicle);
-
-
-            string fileName = "VehicleList.json"; //nazwa pliku
+            string fileName = "VehicleList.txt"; //nazwa pliku
             string filePath = fileName; // Ścieżka do pliku
+
+            var repository = new VehicleRepository();
+            repository.Add(vehicle, filePath, iD);
+
+
+            
 
 
             if (!IsVehicleExists(filePath, vehicle))
             {
-                
-                string jsonString = JsonSerializer.Serialize(vehicle); // Serializacja do JSON
-                File.AppendAllText(filePath, jsonString + Environment.NewLine); // Dopisywanie do pliku
-                Console.WriteLine("Dane dodane do pliku.");
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine($"{vehicle.Id};{vehicle.Maker};{vehicle.Model};{vehicle.GasType};{vehicle.Capacity}");
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Pojazd został dodany.");
+                Console.ForegroundColor = default;
+                Console.ReadKey();
             }
             else
             {
-                Console.WriteLine("zapis już istnieje w pliku.");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Pojazd już istnieje w bazie.");
+                Console.ForegroundColor = default;
+                Console.ReadKey();
             }
-
-
         }
 
+        
         private static bool IsVehicleExists(string filePath, Vehicle vehicle)
         {
             if (!File.Exists(filePath))
@@ -147,27 +204,76 @@ namespace CarRentalConsole
 
                 try
                 {
-                    //var existingVehicle = JsonSerializer.Deserialize<Vehicle>(line);
-                    var existingVehicle = JsonSerializer.Deserialize<Vehicle>(line);
-
-                    if (existingVehicle != null &&
-                        existingVehicle.Maker == vehicle.Maker &&
-                        existingVehicle.Model == vehicle.Model &&
-                        existingVehicle.GasType == vehicle.GasType &&
-                        existingVehicle.Capacity == vehicle.Capacity)
-                        return true;
+                    string[] linesVehicle = File.ReadAllLines(filePath);
+                    foreach (string l in linesVehicle)
+                    {
+                        string[] vehicleData = l.Split(';');
+                        if (vehicleData.Length == 5
+                            //&& int.Parse(vehicleData[0]) == vehicle.Id
+                            && vehicleData[1] == vehicle.Maker
+                            && vehicleData[2] == vehicle.Model
+                            && vehicleData[3] == vehicle.GasType
+                            && int.Parse(vehicleData[4]) == vehicle.Capacity
+                            )
+                            return true;
+                    }
+                    return false;
                 }
-                catch (JsonException)
+                catch(Exception ex)
                 {
-                    Console.WriteLine("Błąd deserializacji linii: " + line);
+                    Console.WriteLine("Błąd " + ex.Message);
                 }
             }
             return false;
         }
 
+        private static int SearcherVehicleId(string filePath, string[] data)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue; //pomijam puste
+
+                try
+                {
+                    string[] linesVehicle = File.ReadAllLines(filePath);
+                    foreach (string l in linesVehicle)
+                    {
+                        string[] vehicleData = l.Split(';');
+                        if (vehicleData.Length == 5
+                            && vehicleData[1] == data[0]
+                            && vehicleData[2] == data[1]
+                            && vehicleData[3] == data[2]
+                            && vehicleData[4] == data[3]
+                            )
+                        {
+                            return int.Parse(vehicleData[0]);
+                        }
+                    }
+                    return 0;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Błąd - SearcherVehicleId: " + ex.Message);
+                }
+                
+            }
+            return 0;
+        }
+
         private static void ShowCars() //pokaż
         {
             VehicleRepository repository = new VehicleRepository(); //zapis TESTOWY!
+
+            //testy
+            
+
+
+
+
+
             var carList = repository.GetAll();
 
             ConsoleTable
@@ -185,6 +291,7 @@ namespace CarRentalConsole
                 "[2] Dodaj pojazd",
                 "[3] Usuń pojazd",
                 "[4] Wypożycz",
+                "[5] Edytuj pojazd",
                 "[0] Zakończ"};
 
             foreach (string op in options)
--- CarRentalConsole/Cars/Repositories/VehicleRepository.cs
+++ CarRentalConsole/Cars/Repositories/VehicleRepository.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CarRentalConsole.Cars.Repositories
@@ -11,37 +10,67 @@ namespace CarRentalConsole.Cars.Repositories
     internal class VehicleRepository
     {
         private static List<Vehicle> data = [];
-        
+        private string nameOfFile = "VehicleList.txt"; //nazwa pliku
 
         public VehicleRepository()
         {
             //TODO: Przerobic na pobiernaie danych z pliku, wrzucić do listy?
+            //1.Sprawdzic czy istnieje
+            Console.WriteLine($"Czy plik istnieje: { File.Exists(nameOfFile)}");
+            //File.Exists(nameOfFile);
+
+
+
         }
 
         public List<Vehicle> GetAll()
         {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            string[] lines = File.ReadAllLines(nameOfFile);
 
-            string readJsonString = File.ReadAllText("VehicleList.json");
-            Vehicle? vehicle = JsonSerializer.Deserialize<Vehicle>(readJsonString);
-            Vehicle loadedVehicle = vehicle;
+            foreach (string line in lines)
+            {
+                // Rozdzielenie danych w linii na podstawie średnika
+                string[] vehicleData = line.Split(';');
 
-          
-            if (data == null)
-                data = [];
-            return data;
+                // Upewnienie się, że dane są poprawne (powinny być 4 elementy)
+                if (vehicleData.Length == 5)
+                {
+                    Vehicle vehicle = new Car //TODO: pojazd jest klasa abstrakcyjną?
+                    {
+                        Id = int.Parse(vehicleData[0]),
+                        Maker = vehicleData[1],
+                        Model = vehicleData[2],
+                        GasType = vehicleData[3],
+                        Capacity = int.Parse(vehicleData[4]) // Konwersja pojemności na liczbę całkowitą
+                    };
+
+                    vehicles.Add(vehicle); // Dodanie pojazdu do listy
+                }
+            }
+            return vehicles; //lista wszytskich pojazdów
         }
         public Vehicle GetVehicle(int id)
         {
             return data.FirstOrDefault(x => x.Id == id); //wyrażenie 'lambda'
         }
 
-        public void Add(Vehicle vehicle)
+        public void Add(Vehicle vehicle, string nameOfFile, int iD = 0)
         {
-            int id = 0;
-            if (data.Any())
-                id = data.OrderByDescending(x => x.Id).First().Id;
-            vehicle.Id = id + 1;
+            int id = iD;
+            string[] lines = File.ReadAllLines(nameOfFile);
+            if (lines.Length > 0)
+            {
+                string[] lastLine = lines[lines.Length - 1].Split(';'); // Pobranie ostatniej linii
+                id = int.Parse(lastLine[0]);
+            }
+
+            //id = data.OrderByDescending(x => x.Id).First().Id;
+            vehicle.Id = iD == 0 ? iD : id + 1; //bo chce nadać przy akt
+
             data.Add(vehicle);
+
+            Console.WriteLine("nadano kolejno ID: "+ vehicle.Id);
 
         }
 
